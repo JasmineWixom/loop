39b219117384891b665dfc1de4d7c162e373260a
Jasmine Wixom <nextechcatapult@MacBook-Air-15.localdomain>
2018-06-26 12:35:27 -0400

    Finished

---
+++ loop.js
@@ -0,0 +1,54 @@
+console.log("1 to 20");
+for(var i = 1; i <= 20; i++) {
+    console.log(i);
+} 
+
+console.log("even1-20");
+for(var i = 0; i <= 20; i+=2) {
+    console.log(i);
+} 
+
+console.log("even1-20");
+for(var i = 0; i <= 20; i+=2) {
+    console.log(i);
+} 
+
+console.log("odd1-20");
+for(var i = 1; i <= 20; i+=2) {
+    console.log(i);
+} 
+
+console.log("squares");
+for(var i = 1; i <= 10; i++) {
+    console.log(i*i);
+} 
+
+console.log("Multi5");
+for(var i = 0; i <= 100; i+=5) {
+    console.log(i);
+} 
+
+console.log("down1");
+for (i = 20; i >= 0; i-=1) {
+console.log(i);
+}
+
+console.log("downeven1");
+for (i = 20; i >= 0; i-=2) {
+console.log(i);
+}
+
+console.log("downodd1");
+for (i = 19; i >= 0; i-=2) {
+console.log(i);
+}
+
+console.log("down5");
+for (i = 100; i >= 0; i-=5) {
+console.log(i);
+}
+
+console.log("squaredown");
+for (i = 10; i >= 0; i-=1) {
+console.log(i*i);
+}
